f70e7a38213d133f2f6bdd26d3ad89c3f6badc42
hristijan1 <hristijan@digitalnode.com>
2022-11-22 16:57:52 +0100

    Feat: extra modal warning before clearing an index/leaving page while reindexing

--- admin/src/js/wordpress-meilisearch-admin.js
+++ admin/src/js/wordpress-meilisearch-admin.js
@@ -1,10 +1,15 @@
 import axios from  'axios';
+import Swal from 'sweetalert2'
 
 // Counts valid indexed posts.
 var valid       = 0;
 
 // Counts corrupted indexed posts.
 var corrupted   = 0;
+
+// Indexing status. Could be either idle (0) or active (1).
+var status      = 0;
+var warningExitTimesShown = 0;
 
 (function( $ ) {
 	'use strict';
@@ -27,6 +32,8 @@ var corrupted   = 0;
 
             $("#error-logs-parent").removeClass('blur-lg cursor-not-allowed');
 
+            status = 1;
+
             start_reindex( 'start_reindex', index, 0, evt.target, statusBadge );
         });
 
@@ -35,7 +42,28 @@ var corrupted   = 0;
 
             let index = $(evt.target).attr('data-index')
 
-            clear_index( 'clear_index', index );
+            Swal.fire({
+                title: 'Are you sure you want to clear the index?',
+                text: 'Once you clear the index there is no going back, think twice before proceeding 😉',
+                confirmButtonColor: '#dc2626',
+                showCancelButton: true,
+                confirmButtonText: '🗑️ Clear Index',
+            }).then((result) => {
+                /* Read more about isConfirmed, isDenied below */
+                if (result.isConfirmed) {
+                    clear_index( 'clear_index', index );
+                }
+            })
+        });
+
+        $( document ).mouseleave( () => {
+            if ( status && ! warningExitTimesShown++ ){
+                Swal.fire(
+                    'Reindexing is not finished',
+                    'The process of reindexing is not complete, you will have to start it again to finish it completetly in future.',
+                    'info'
+                )
+            }
         });
     });
 
@@ -73,6 +101,8 @@ var corrupted   = 0;
                     }).addClass('bg-green-100 text-green-700');
                     progressBar.removeClass('bg-orange-600').addClass('bg-green-500')
                 }
+
+                status = 0;
             }
         } catch(e){
             start_reindex( action, index, offset, statusBadge );
