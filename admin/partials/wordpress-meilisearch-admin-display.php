62484d4183df26e6ab21762ed3ab2c61de242a71
hristijan1 <hristijan@digitalnode.com>
2022-11-23 12:31:30 +0100

    Display all CPTs in the dashboard

--- admin/partials/wordpress-meilisearch-admin-display.php
+++ admin/partials/wordpress-meilisearch-admin-display.php
@@ -13,13 +13,13 @@
  */
 ?>
 
-<div class="plugin-dashboard-wrapper px-4 sm:px-6 lg:px-8">
+<div class="plugin-dashboard-wrapper">
 
     <!-- Compiling Tailwind classes so we can use them dynamically -->
     <template class="hidden bg-gray-600 bg-orange-600 bg-green-100 text-white text-orange-100 text-green-700 bg-green-500"></template>
     <div class="grid lg:grid-cols-4 gap-12 lg:gap-16">
         <div class="plugin-actions lg:col-span-3">
-            <h2 class="mt-8 max-w-6xl text-lg font-medium leading-6 text-gray-900">All Indexes</h2>
+            <h2 class="mt-8 max-w-6xl text-xl mb-4 font-medium leading-6 text-gray-900">All Indexes</h2>
             <!-- Activity table (small breakpoint and up) -->
             <div class="mt-2 flex flex-col">
                     <div class="min-w-full overflow-hidden overflow-x-auto align-middle shadow sm:rounded-lg">
@@ -33,49 +33,49 @@
                             </tr>
                             </thead>
                             <tbody class="divide-y divide-gray-200 bg-white">
-                            <tr class="bg-white align-baseline">
-                                <td class="w-full max-w-0 whitespace-nowrap px-6 py-4 text-sm text-gray-900 align-middle">
-                                    <div class="group flex items-center space-x-8 truncate text-sm">
-                                        <div class="flex space-x-2">
-                                            <!-- Heroicon name: mini/banknotes -->
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-gray-500">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125z" />
-                                            </svg>
+                                <?php foreach ( $cpts ?? [] as $cpt ){
+                                    $icon = get_post_type_object( $cpt )->menu_icon;
+                                ?>
+                                    <tr class="bg-white align-baseline">
+                                        <td class="w-full max-w-0 whitespace-nowrap px-6 py-4 text-sm text-gray-900 align-middle">
+                                            <div class="group flex items-center space-x-8 truncate text-sm">
+                                                <div class="flex space-x-2">
+                                                    <div class="wp-menu-image dashicons-before <?= isset($icon) && strlen($icon) ? $icon : 'dashicons-admin-page' ?>"></div>
 
-                                            <p class="truncate text-gray-500 group-hover:text-gray-900">Products</p>
-                                        </div>
+                                                    <p class="truncate text-gray-500 group-hover:text-gray-900 capitalize"><?= $cpt ?></p>
+                                                </div>
 
-                                        <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 hidden">
-                                            <div data-index="item" class="progress bg-orange-600 text-xs font-medium text-orange-100 text-center p-0.5 leading-none rounded-full" style="width: 45%"> 45%</div>
-                                        </div>
-                                    </div>
+                                                <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700 hidden">
+                                                    <div data-index="<?= esc_attr( $cpt ) ?>" class="progress bg-orange-600 text-xs font-medium text-orange-100 text-center p-0.5 leading-none rounded-full" style="width: 45%"> 45%</div>
+                                                </div>
+                                            </div>
 
-                                </td>
-                                <td class="hidden whitespace-nowrap px-6 py-4 text-sm text-gray-500 md:block">
-                                    <span
-                                        data-index="item" class="status-badge inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-600 text-white capitalize ">
-                                        idle
-                                    </span>
-                                </td>
-                                <td class="whitespace-nowrap px-6 py-4 text-right text-sm text-gray-500">
-                                    <button
-                                            class="disabled:opacity-75 disabled:cursor-not-allowed start-reindex inline-flex items-center rounded-full border border-transparent bg-indigo-600 px-3 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                            data-index="item"
-                                    >
-                                        Reindex
-                                    </button>
-                                    <button
-                                            class="disabled:opacity-75 disabled:cursor-not-allowed clear-index inline-flex items-center rounded-full border border-transparent bg-red-600 px-3 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                                            data-index="item"
-                                    >
-                                        Clear
-                                    </button>
-                                </td>
-                                <td class="whitespace-nowrap px-6 py-4 text-right text-sm text-gray-500">
-                                    <time datetime="2020-07-11">July 11, 2020</time>
-                                </td>
-                            </tr>
-
+                                        </td>
+                                        <td class="hidden whitespace-nowrap px-6 py-4 text-sm text-gray-500 md:block">
+                                            <span
+                                                data-index="<?= esc_attr( $cpt ) ?>" class="status-badge inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-600 text-white capitalize ">
+                                                idle
+                                            </span>
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4 text-right text-sm text-gray-500">
+                                            <button
+                                                    class="disabled:opacity-75 disabled:cursor-not-allowed start-reindex inline-flex items-center rounded-full border border-transparent bg-indigo-600 px-3 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                                    data-index="<?= esc_attr( $cpt ) ?>"
+                                            >
+                                                Reindex
+                                            </button>
+                                            <button
+                                                    class="disabled:opacity-75 disabled:cursor-not-allowed clear-index inline-flex items-center rounded-full border border-transparent bg-red-600 px-3 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                                    data-index="<?= esc_attr( $cpt ) ?>"
+                                            >
+                                                Clear
+                                            </button>
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4 text-right text-sm text-gray-500">
+                                            <time datetime="2020-07-11">July 11, 2020</time>
+                                        </td>
+                                    </tr>
+                                <?php } ?>
                             </tbody>
                         </table>
                     </div>
