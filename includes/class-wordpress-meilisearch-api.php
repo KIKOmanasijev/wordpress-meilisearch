c80e02f2d3b12de42129a552095349216f9b0134
hristijan1 <hristijan@digitalnode.com>
2023-02-28 13:22:14 +0100

    Refactoring class-wordpress-melisearch-api.php

--- includes/class-wordpress-meilisearch-api.php
+++ includes/class-wordpress-meilisearch-api.php
@@ -35,35 +35,19 @@ class Wordpress_Meilisearch_Api {
 	}
 
 	function wordpress_meilisearch_fetch( WP_REST_Request $request ) {
-		$posts_per_page = intval( $request['posts_per_page'] ?? 18 );
-		$page = abs( intval( $request['current_page'] ?? 1 ) - 1 );
+		$search_params = $this->extract_search_params_from_request($request);
 
-		$search = $request['q'] ?? '';
-		$sort_by = $request['sort_by'] ?? 'post_date:desc';
-		$post_type = $request['post_type'];
-
-		$facet_data = $this->client->index( $post_type )->search(
-			'',
-			[ 'facets' => ['*'] ]
-		);
+		// Run 'empty' query so we can get all possible facet values. Needs to be cached in the future.
+		$facet_data = $this->client->index( $search_params['post_type'] )->search('',[ 'facets' => ['*'] ]);
 
 		// Filterable attributes for the index.
 		$filterable_attributes = $this->client->index('product')->getSettings()['filterableAttributes'];
 
-		$request_params = $request->get_query_params();
+		// This is needed since PHP is replacing '.' with '_' in the query param keys.
+		$request_params = $this->replace_underscore_suffix_with_dot_in_array_keys($request->get_query_params());
 
-		foreach ( $request_params as $key => $value ){
-			if ( str_contains($key, '_hierarchical_lvl') ){
-				unset($request_params[$key]);
-				$request_params[str_replace('hierarchical_lvl', 'hierarchical.lvl', $key)] = $value;
-			}
-		}
-
-		$params = array_filter( $request_params, function ( $value, $key ) use ( $filterable_attributes ) {
-			return in_array( $key, $filterable_attributes ) ||
-			       ( str_starts_with( $key, 'range-min' ) || str_starts_with( $key, 'range-max' ) || in_array( substr( $key, 10 ), $filterable_attributes ) )
-				;
-		}, ARRAY_FILTER_USE_BOTH );
+		// Get only the params that will be needed to build the Filter args array. (basically get only existing properties)
+		$params = $this->get_filterable_params_from_search_query($request_params, $filterable_attributes);
 
 		$filters = [];
 
@@ -71,38 +55,46 @@ class Wordpress_Meilisearch_Api {
 		foreach ( $params as $key => $value ){
 			// Checkboxes (any multiple choice field) value extraction.
 			if ( gettype( $value ) == 'array' ){
-				$arrayOfOrs = array_map(
-					function ( $item ) use ( $key ) {
-						return sprintf( "%s = '%s'", $key,  $item );
-					},
-					$value
-				);
+				if ( str_contains($key, '_hierarchical') ){
+					$lvlPos = strpos($key, '.lvl');
+					$keyWithoutLvlSuffix = substr($key, 0, $lvlPos);
+					$arrayOfOrs = [];
 
-				$filters[] = $arrayOfOrs;
+					foreach ( $params as $sec_loop_key => $sec_loop_value ){
+						if (str_starts_with($sec_loop_key, $keyWithoutLvlSuffix)){
+							$arrayOfOrs = array_merge(
+								$arrayOfOrs,
+								$this->generate_array_of_ors($sec_loop_value, $sec_loop_key)
+							);
+
+							unset($params[$sec_loop_key]);
+						}
+					}
+
+					if ( $arrayOfOrs )
+						$filters[] = $arrayOfOrs;
+				} else {
+					// Generate array of ORs from one property - ex: from category_hierarchical.lvl1
+					$filters[] = $this->generate_array_of_ors( $value, $key );
+				}
 			// Search box, radio buttons (any single choice/value field), min-max value extraction.
-			} else if ( gettype( $value ) == 'string' && strlen( $value ) )
-				if ( str_starts_with( $key, 'range-min' ) ) {
-					$filters[] = sprintf( "%s > %s", substr( $key, 10 ), $value );
-				} else if ( str_starts_with( $key, 'range-max' ) ){
-					$filters[] = sprintf( "%s < %s", substr( $key, 10 ), $value );
-				}
-				else {
-					$filters[] = sprintf( "%s = '%s'", $key,  $value );
-				}
+			} else if ( gettype( $value ) == 'string' && strlen( $value ) ){
+				$filters[] = $this->generate_filters_for_simple_facets($key, $value);
+			}
 
 			unset( $params[ $key ] );
 		}
 
 		$options = [
-			'sort'   => [ $sort_by ],
+			'sort'   => [ $search_params['sort_by'] ],
 			'facets' => [ '*' ],
-			'offset' => $posts_per_page * $page,
-			'limit'  => $posts_per_page,
+			'offset' => $search_params['posts_per_page'] * $search_params['page'],
+			'limit'  => $search_params['posts_per_page'],
 			'filter'  => $filters
 		];
 
-		$results = $this->client->index( $post_type )->search(
-			$search,
+		$results = $this->client->index( $search_params['post_type'] )->search(
+			$search_params['search'],
 			$options
 		);
 
@@ -112,14 +104,23 @@ class Wordpress_Meilisearch_Api {
 			echo \Roots\View('partials.meilisearch.archives.items', [
 				'results' => $results->getHits(),
 				'total_hits' => $results->getEstimatedTotalHits(),
-				'total_pages' => ceil( $results->getEstimatedTotalHits() / $posts_per_page ),
+				'total_pages' => ceil( $results->getEstimatedTotalHits() / $search_params['posts_per_page'] ),
 				'response' => $results,
-				'page' => $page + 1,
-				'sort_by' => $sort_by,
-				'facets' => $facet_data->getFacetDistribution()
+				'page' => $search_params['page'] + 1,
+				'sort_by' => $search_params['sort_by'],
+				'facets' => $facet_data->getFacetDistribution(),
+				'params' => $request_params
 			] )->render();
 		} else {
-			return new WP_Error( 400, 'No products found' );
+			echo \Roots\View('partials.meilisearch.archives.items', [
+				'results' => [],
+				'total_hits' => 0,
+				'total_pages' => 1,
+				'page' => $search_params['page'] + 1,
+				'sort_by' => $search_params['sort_by'],
+				'facets' => $facet_data->getFacetDistribution(),
+				'params' => $request_params
+			] )->render();
 		}
 
 		die;
@@ -154,5 +155,54 @@ class Wordpress_Meilisearch_Api {
 		);
 
 		return apply_filters("meilisearch_${index}_modify_sort_options", $results );
+	}
+
+	private function generate_array_of_ors($elements, $propertyName): array{
+		return array_map(
+			function ( $item ) use ( $propertyName ) {
+				return sprintf( "%s = '%s'", $propertyName,  $item );
+			},
+			$elements
+		);
+	}
+
+	private function replace_underscore_suffix_with_dot_in_array_keys(array $params): array {
+		foreach ( $params as $key => $value ){
+			if ( str_contains($key, '_hierarchical_lvl') ){
+				unset($params[$key]);
+				$params[str_replace('hierarchical_lvl', 'hierarchical.lvl', $key)] = $value;
+			}
+		}
+
+		return $params;
+	}
+
+	private function extract_search_params_from_request( WP_REST_Request $request ): array {
+		return [
+			'posts_per_page' => intval( $request['posts_per_page'] ?? 18 ),
+			'page'           => abs( intval( $request['current_page'] ?? 1 ) - 1 ),
+			'search'         => $request['q'] ?? '',
+			'sort_by'        => $request['sort_by'] ?? 'post_date:desc',
+			'post_type'      => $request['post_type']
+		];
+	}
+
+	private function get_filterable_params_from_search_query($request_params, $filterable): array {
+		return array_filter( $request_params, function ( $value, $key ) use ( $filterable ) {
+			return in_array( $key, $filterable ) ||
+			       ( str_starts_with( $key, 'range-min' ) || str_starts_with( $key, 'range-max' ) || in_array( substr( $key, 10 ), $filterable ) )
+				;
+		}, ARRAY_FILTER_USE_BOTH );
+	}
+
+	private function generate_filters_for_simple_facets( int $key, $value ) {
+		if ( str_starts_with( $key, 'range-min' ) ) {
+			return sprintf( "%s > %s", substr( $key, 10 ), $value );
+		} else if ( str_starts_with( $key, 'range-max' ) ){
+			return sprintf( "%s < %s", substr( $key, 10 ), $value );
+		}
+		else {
+			return sprintf( "%s = '%s'", $key,  $value );
+		}
 	}
 }
